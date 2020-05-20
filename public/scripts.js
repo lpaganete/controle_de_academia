01fdf032d4b484545491c9fc191f8bb56c6998fc
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-05-20 16:35:29 -0300

    Adiconando a class active nos links do menu dinamicamente

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,13 @@
+const currentPage = location.pathname //localizando a página em que estou
 
-// o objeto document consegue manipular todos os elementos e  tags do html transformando cada tag em objeto 
-// O querySelector vai selecionar qualquer elemento baseado em classe, id, ou no proprio nome da tag. (Baseado no CSS )
+//pegando os itens do menu
+const menuItems = document.querySelectorAll("header .links a") 
 
-/*ABRINDO O MODAL*/
-const modalOverlay = document.querySelector('.modal-overlay') //Aqui estou selecionando a classe moda-overlay do css. Quando ele achar, ele vai adicionar na variavel modalOverlay que foi criada. 
-const  cards =  document .querySelectorAll ('.card') //com o all, ele irá pegar todos os elementos com classe card 
+//este for percorre os links do menu e se a localização em que estivermos for igual a do href, ele adiciona uma class active que muda o css do link
+for (item of menuItems) {
+   if(currentPage.includes(item.getAttribute("href"))) {
+      item.classList .add("active") 
+   }
+}
 
-//Para cada card dentro da variavel cards. qundo "ouvir" um click. chame a classe CSS: modal-overlay.active 
-for (let card of cards) {
-    //addEventListener é um ouvidor de eventos. O evento que vou querer ouvir é um click
-     card.addEventListener("click", function() {
-        
-        const videoId = card.getAttribute("id") //! getAttribute, permite pegar qualquer atributo dentro da div 
 
-        //Dizendo para que página que eu quero que vá quando clicar no card
-        window.location.href = `/video?id=${videoId}`  
- 
-     }) 
-} 
